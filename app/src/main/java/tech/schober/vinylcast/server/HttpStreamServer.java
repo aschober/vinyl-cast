51d4889bc1d489498afc3394d85a4544a7038917
Kyle Smith <kyle@makrs.org>
2023-12-14 22:39:14 -0500

    Use Vinyl Cast image as "album art" when casting.

--- app/src/main/java/tech/schober/vinylcast/server/HttpStreamServer.java
+++ app/src/main/java/tech/schober/vinylcast/server/HttpStreamServer.java
@@ -4,11 +4,13 @@ import java.io.IOException;
 
 public interface HttpStreamServer {
     String HTTP_SERVER_URL_PATH = "/vinylcast";
+    String HTTP_SERVER_IMAGE_PATH = "/image.webp";
     int HTTP_SERVER_PORT = 8080;
 
     void start() throws IOException;
     void stop();
     String getStreamUrl();
+    String getImageUrl();
     String getContentType();
     int getClientCount();
     void addServerListener(HttpStreamServerListener listener);
